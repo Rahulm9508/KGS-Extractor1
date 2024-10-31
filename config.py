a8d9daab5e484875c93716939b137852acba3de1
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2024-11-01 01:04:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = 20346550
 api_hash = "bc79c3bea7a626887bdc0871eecf0327"
-bot_token = "7289136166:AAGPpHK4yz99XD5x6JJixPyTlHZvjlFC5aI"
+bot_token = "7844778446:AAHV_UwNtY_sgRFW0yemzdeBCSItY7Fpo00"
 auth_users = "7081036509"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "7081036509"
